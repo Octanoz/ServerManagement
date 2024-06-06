5164553c3ba1608a895030a4a9dc59d37f07c09f
Octanoz <130052848+Octanoz@users.noreply.github.com>
2024-06-06 07:04:29 +0000

    Fixed typo in CitiesRepository

--- PracticeSSR/Models/CitiesRepository.cs
+++ PracticeSSR/Models/CitiesRepository.cs
@@ -5,7 +5,7 @@ public static class CitiesRepository
     private static readonly List<City> cities =
     [
         new() { Name = "Amsterdam", Country = "Netherlands", Image = @"\images\a-dam-canal-houses.svg" },
-        new() { Name = "Eindhoven", Country = "Netherlands", Image = @"\images\Evoluon.svg" },
+        new() { Name = "Eindhoven", Country = "Netherlands", Image = @"\images\evoluon.svg" },
         new() { Name = "Paris", Country = "France", Image = @"\images\eiffel-tower.svg" },
         new() { Name = "Rome", Country = "Italy", Image = @"\images\colosseum.svg" },
         new() { Name = "San Francisco", Country = "USA", Image = @"\images\golden-gate.svg" },
